5bb799d84c5e41586c3dcd39cf920f60f79655bd
Nimera36 <unimera1@stu.vistula.edu.pl>
2021-10-14 17:33:11 +0300

    1

--- src/main/java/pl/vistula/helloworldNimera/HelloWorldNimera.java
+++ src/main/java/pl/vistula/helloworldNimera/HelloWorldNimera.java
@@ -3,6 +3,7 @@ package pl.vistula.helloworldNimera;
 public class HelloWorldNimera {
     public static void main(String[] args) {
         System.out.println("Hello Vistula Nimera");
-        
+        System.out.println("Hello World");
+        System.out.println("Jakiś text");
     }
 }
